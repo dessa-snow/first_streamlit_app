169f1b51b9834fd8c178abbe53bbb64dcf6cee7d
dessa-snow <110003475+dessa-snow@users.noreply.github.com>
2022-07-26 11:24:51 +0800

    Read csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My Parents New Healthy Diner")
 
@@ -9,3 +10,6 @@ st.caption(":egg: Hard-boiled Free-Range Egg")
 st.caption(":avocado: Avocado Toast")
 
 st.subheader(":banana: :grapes: Build Your Own Fruit Smoothie :watermelon: :strawberry:")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
