c178c08a68526ddfebb5470378b7c8f7e8a1d62b
dessa-snow <110003475+dessa-snow@users.noreply.github.com>
2022-07-26 18:20:10 +0800

    Fix errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
 
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
 
 st.subheader("Fruityvice Fruit Advice!")
