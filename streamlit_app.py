743450863bd5dfca63b56483f59f723c77c4b9ad
dessa-snow <110003475+dessa-snow@users.noreply.github.com>
2022-07-26 18:22:33 +0800

    Fix errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+st.write('The user entered ', fruit_choice)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
