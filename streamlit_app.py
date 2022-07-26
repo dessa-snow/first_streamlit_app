f3fb7787a523fd27d7f415f8e1e1d956f900e57d
dessa-snow <110003475+dessa-snow@users.noreply.github.com>
2022-07-26 10:24:24 +0800

    Add subheadings and captions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-import streamlit
+import streamlit as st
 
-streamlit.title("My Parents New Healthy Diner")
+st.title("My Parents New Healthy Diner")
+
+st.subheader("Breakfast Menu")
+st.caption("Omega 3 & Blueberry Oatmeal")
+st.caption("Kale, Spinach & Rocket Smoothie")
+st.("Hard-boiled Free-Range Egg")
