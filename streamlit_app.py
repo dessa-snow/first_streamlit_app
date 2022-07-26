b41ebad72aba555f0897472e00421e8a146fc001
dessa-snow <110003475+dessa-snow@users.noreply.github.com>
2022-07-26 10:34:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ st.caption(":bowl_with_spoon: Omega 3 & Blueberry Oatmeal")
 st.caption(":green_salad: Kale, Spinach & Rocket Smoothie")
 st.caption(":egg: Hard-boiled Free-Range Egg")
 st.caption(":avocado: Avocado Toast")
+
+st.subheader(":banana: :grapes: Build Your Own Fruit Smoothie :kiwi: :strawberry:")
