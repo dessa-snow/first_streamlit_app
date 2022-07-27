512ee9834069b0ebaa2281a6e2bab62633880b3f
dessa-snow <110003475+dessa-snow@users.noreply.github.com>
2022-07-27 10:07:10 +0800

    Fix spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.dataframe(fruits_to_show)
 # st.text(fruityvice_response.json())
 
 # create function
-def get_fruitvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
